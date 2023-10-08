092b17cbe3c7b26c8cdc2c8d5ff7ccfd518af972
Niklas Schönberg <niklas@uplink.fi>
2023-10-08 11:37:12 +0300

    Lazyvim base

--- init.lua
+++ init.lua
@@ -1,11 +1,2 @@
-vim.opt.number = true
-
-require('options')
-
-require('keymaps')
-
-require('plugins')
-
-require('theme')
-
-vim.cmd([[silent! colorscheme tokyonight]])
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
